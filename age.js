4d7b079333c9030345669162c5d31ade27a3c0d7
rkdd51 <rahuldubay51@gmail.com>
2021-03-12 20:48:42 +0530

    JS part remaining

---
+++ age.js
@@ -0,0 +1,38 @@
+//Calculate Age
+
+// let daysH= document.getElementById("date");
+// let monthH= document.getElementById("month");
+// let yearH= document.getElementById("year");
+
+// let showAge= document.getElementById("show-age");
+
+// const btn = document.getElementById("calc-btn");
+
+// var today = new Date();
+
+
+// let d=today.getDate();
+// let m=today.getMonth() + 1;
+// let y=today.getFullYear();
+
+// let maxDays =0;
+// let monthNum=1;
+
+// btn.addEventListener("click",(days,month,year)=>{
+
+//     days=dayH.value;
+//     month=monthH.value;
+//     year=yearH.value; 
+
+//     if (dateH.value) {
+        
+//     }
+// }
+
+const year=[1991,2003,1969,2019];
+const ages=[];
+
+for (let i=0; i<years.length; i++) {
+    ages.push(2021-years[i]);
+}
+document.write(ages);
